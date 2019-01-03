ed2e51a38fa8ae8e64bffc54c0fd653b9c62f691
Alan Saber <battlecraft95@gmail.com>
2019-01-03 23:36:12 +0100

    Add configuration and react

---
+++ src/main/js/app.js
@@ -0,0 +1,18 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+    );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
